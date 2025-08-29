d361a9abdaf78d4d3b6b7c294d6a0c3e5bf69026
pedrohenry <faquinipedro12@gmail.com>
2025-08-29 09:55:27 -0300

    .

---
+++ react-projeto/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+export default function Profile (){
+    return (
+      <div className="profile">
+            <img
+              src="https://github.com/pedrohenry12.png"
+              alt="avatar do Rafael"
+              className="profile-avatar"
+            />
+            <h1 className="profile-name">Pedro Henry</h1>
+            <p className="profile-bio">Desenvolvedor de Sistemas</p>
+          </div>
+    )
+  }
+
